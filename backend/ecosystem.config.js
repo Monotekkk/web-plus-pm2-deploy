18ab01554e98f444ec68d71921dda7c47631879a
Timur Yakhin <timur.yakhin.99@gmail.com>
2024-08-27 13:54:50 +0300

    fix deploy

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,13 +1,14 @@
 const dotenv = require('dotenv');
-dotenv.config({path: "./.env.deploy"})
+
+dotenv.config({ path: './.env.deploy' });
 
 const {
-  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPOSITORY
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPOSITORY,
 } = process.env;
 module.exports = {
-  apps : [{
-    name   : "mestox",
-    script : "./dist/app.js"
+  apps: [{
+    name: 'mesto',
+    script: './dist/app.js',
   }],
   deploy: {
     production: {
@@ -17,7 +18,7 @@ module.exports = {
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
       'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
-      'post-deploy': 'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env productionpm 2',
+      'post-deploy': 'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
-}
+};

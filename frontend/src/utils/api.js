b383516630e1e37dbd3b4c96bc69ecd70c758d0f
Timur Yakhin <timur.yakhin.99@gmail.com>
2024-08-27 15:39:34 +0300

    push fix

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('https://api.monotek.nomorepartiesco.ru/');
+  const api = new Api('https://api.monotek.nomorepartiesco.ru');
 
   export default api;
